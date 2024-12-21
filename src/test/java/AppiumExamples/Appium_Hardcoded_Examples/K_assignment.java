4557cf53a7ca1bed1a43d6a6b019f3967739afb8
aelaina100 <aelaina100@gmail.com>
2024-12-21 01:19:12 -0500

    Finalizing swiping & drag and droping .java class files

---
+++ src/test/java/AppiumExamples/Appium_Hardcoded_Examples/K_assignment.java
@@ -0,0 +1,31 @@
+package AppiumExamples.Appium_Hardcoded_Examples;
+
+import org.testng.annotations.Test;
+
+public class K_assignment {
+	
+	@Test
+	public void test1()
+	{
+		
+	}
+	
+	
+	
+	
+	@Test
+	public void test2()
+	{
+		
+	}
+	
+	
+	
+	
+	@Test
+	public void test3()
+	{
+		
+	}
+
+}
